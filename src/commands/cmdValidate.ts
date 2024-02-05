27e26ad2326cf7b6e13a93d7a1284b54323fdb28
jared <jaredgold@proton.me>
2024-02-05 00:13:15 -0500

    set up base cmd files

--- src/commands/cmdValidate.ts
+++ src/commands/cmdValidate.ts
@@ -1,1 +1,7 @@
 const validCmds : string[] = ['login', 'start', 'stop', 'info', 'project'];
+
+const validate = (array : string[], cmd : string) => {
+
+}
+
+
--- src/commands/commands.ts
+++
@@ -1,6 +0,0 @@
-const login = (user : string, pass : string) => {
-  console.log(user + " user");
-  console.log(pass + " pass");
-}
-
-export { login };
