9116bce8ae38ab857a07b45c0df34d655f16763c
jared <114547220+goldjared@users.noreply.github.com>
2024-02-05 00:13:15 -0500

    set up base cmd files

--- src/commands/cmdProcessor.ts
+++ src/commands/cmdProcessor.ts
@@ -1,14 +1,26 @@
 import { argv } from 'node:process';
-import { login } from './commands'
+import { login } from './login'
 
 // print process.argv
 const processor = () => {
+/*
 argv.forEach((val, index) => {
   console.log(`${index}: ${val}`);
+  if(val === 'login') {
+        
+  }
 });
+*/
+  const command : string = argv[2]; 
+  // if there is only 1 input, and input is valid command, display help for said cmd
+  if(argv.length === 3) {
 
+  }
+  if(command === "login") {
+    // validate the argv array for the matching command
+    validate(argv, command);
+    login(argv[2], argv[3]);
+  }
 }
-
-login(argv[2], argv[3]);
 
 export { processor } 
