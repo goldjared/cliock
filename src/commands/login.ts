52fc382cfc97d3b1514b2bbae174a05f650dbd36
jared <114547220+goldjared@users.noreply.github.com>
2024-02-12 21:45:49 -0500

    move login logic from process. to login file

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,7 +1,33 @@
-const login = (user: string, pass: string) => {
-console.log(user);
-console.log(pass);
+import { clockifyResponse, genReqOptions } from "../clockifyApi/validation";
+import type {
+  UserProfile,
+  ApiValid,
+  ApiInvalid,
+} from "../clockifyApi/validation";
+import { writeApi } from "../sys/fileOps";
 
-}
+const userUrl: string = "https://api.clockify.me/api/v1/user";
 
-export { login }
+const login = (apiKey: string): boolean => {
+  clockifyResponse(userUrl, genReqOptions(apiKey))
+    .then((data: ApiValid | ApiInvalid | UserProfile) => {
+      // type guard
+      if ("name" in data) {
+        //         const responseLen: number = Object.keys(data).length;
+        writeApi(apiKey);
+        console.log(
+          `Key authenticated. '${data.name}' Logged in successfully.`
+        );
+        return true;
+      } else {
+        console.log("Invalid. Authentication failed");
+      }
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the execution
+      console.error("Error:", error);
+    });
+  return false;
+};
+
+export { login };
