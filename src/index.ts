27e57f7f8a9f7fc083e72daddc771d674b7ff1f7
jared <jaredgold@proton.me>
2024-02-15 01:51:20 -0500

    format

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 import { processor } from "./commands/processor";
-import { fs, homeDir, writeApi } from "./sys/fileOps";
+import { fs, homeDir, writeData } from "./sys/fileOps";
 
-// check if .cliock exists, if not, create file.  
+// check if .cliock exists, if not, create file.
 fs.readFile(homeDir + "/.cliock", "utf8", (err: unknown, data: string) => {
   if (err) {
-    writeApi("");
+    writeData("");
     console.log("Set up complete. Type 'iok login <Clockify_API_key_here>'");
     return;
   }
+  // file exists, get data from file, call processor with data
   let api: string = data;
   api = api.slice(0, -1);
   processor(api);
