4ad3c283aea05d6191000d3564db78a73af3b53a
jared <jaredgold@proton.me>
2024-02-22 00:26:53 -0500

    create stop method, clean up unused imports and code

--- src/commands/timer.ts
+++ src/commands/timer.ts
@@ -1,25 +1,8 @@
-import { readData } from "../sys/fileOps";
-import type {
-  ProjectList,
-  UserData,
-  Timer,
-} from "../clockifyApi/validation";
+import { readData, writeData } from "../sys/fileOps";
+import type { UserData } from "../clockifyApi/validation";
+
 const getWorkspaceTimeUrl = (wrkspcId: string): string =>
   `https://api.clockify.me/api/v1/workspaces/${wrkspcId}/time-entries`;
-
-/*
-  
-start <project> 
-record in json project, start-time, ""/null (end time) 
-
-stop 
-check file for time json 
-if json is there, adds end: <end-time> 
-then sends post to clockify with time entry, 
-wipes time entry from file
-
-
- */
 
 const getProjectId = (projectName: string): string => {
   if (readData() === "") {
@@ -71,4 +54,18 @@ const start = (projectName: string, projectId: string): void => {
   };
 };
 
-export { getProjectId, start };
+const stop = (): void => {
+  if (readData() === "") {
+    // display start up message
+    console.log("No saved data found. Creating file...");
+    return;
+  }
+  const userData: string = readData();
+  const userDataJson: UserData = JSON.parse(userData);
+  userDataJson.timer.end = getUTCTimeNow();
+  // send the POST request with the timer.
+  // set .timer to blank values, then write it to file.
+  writeData(JSON.stringify());
+};
+
+export { getProjectId, start, stop };
