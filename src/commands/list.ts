8e047797874f0d655c5ed5c38f5a66ee4ec84132
jared <jaredgold@proton.me>
2024-02-27 12:21:10 -0500

    refactor file utilizing getters and display methods, create separate method for get project at index

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,42 +1,70 @@
-import type { ProjectList } from "../clockifyApi/validationTypes";
+import type {
+  Project,
+  ProjectList,
+  UserData,
+  UserProfile,
+} from "../clockifyApi/validationTypes";
 import { readData } from "../sys/fileOps";
 
-const listProjects = (index: string | undefined = undefined): void => {
-  const data = JSON.parse(readData());
-  if (data === "") {
-    console.log(
-      "No projects found for current workspace. Login with 'iok login <API_KEY>'"
-    );
+// displays active workspae and total projects on wrkspace
+const displayProjectHeader = (
+  userData: UserProfile,
+  projListLeng: number
+): void => {
+  console.log(
+    "Workspace ID:",
+    userData.activeWorkspace,
+    "Projects: " + projListLeng
+  );
+  console.log("---------------------------------------------");
+};
+
+const displayProject = (proj: Project, prefix?: string): void => {
+  prefix !== undefined
+    ? console.log(prefix + ":", proj.name)
+    : console.log(proj.name);
+};
+
+const listProjectAtIndex = (index: string): Project | undefined => {
+  const userData: UserData = JSON.parse(readData());
+  const projList: ProjectList = userData.projects;
+  if (projList === undefined) {
+    console.log("No projects found");
+    return undefined;
   }
-  const projList: ProjectList = data.projects;
   const projListLength: number = Object.keys(projList).length;
   // check if index val is present, and isNaN(+index) returns true if NaN
   if (index !== undefined && isNaN(+index)) {
     console.log("Project index value entered '" + index + "' is not a number.");
-    return;
+    return undefined;
   }
 
   const indexNum: number = Number(index) - 1;
-  if (indexNum > projListLength || indexNum < 0) {
-    console.log("Entered index value out of range");
-    return;
+  if (indexNum >= projListLength || indexNum < 0) {
+    console.log(
+      "Entered index value out of range, type 'cliok list' for index values"
+    );
+    return undefined;
   }
 
   if (projList[indexNum] !== null) {
-    console.log(projList[Number(index) - 1].name);
-    return;
-  }
-
-  console.log(
-    "Workspace ID:",
-    data.userProfile.activeWorkspace,
-    "Projects: " + projListLength
-  );
-  console.log("---------------------------------------------");
-  // iterate through project list, return proj ID if name param exists in projList
-  for (let i = 0; i < projListLength; i++) {
-    console.log(i + 1 + ":", projList[i].name);
+    displayProject(projList[indexNum]);
   }
 };
 
-export { listProjects };
+// lists all projects saved to file
+const listProjects = (): void => {
+  const userData: UserData = JSON.parse(readData());
+  const projList: ProjectList = userData.projects;
+  if (projList === undefined) {
+    console.log("No projects found");
+    return;
+  }
+  const projListLength: number = Object.keys(projList).length;
+  displayProjectHeader(userData.userProfile, projListLength);
+  for (let i = 0; i < projListLength; i++) {
+    displayProject(projList[i], i + 1 + "");
+  }
+};
+
+export { listProjects, listProjectAtIndex };
