3866181ebb6a32d6e869268ce29bae5859bb503e
jared <jaredgold@proton.me>
2024-02-29 00:24:12 -0500

    improve display msgs in help, utilize isValid method in place of Map.has, improve command data in map

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,8 +1,9 @@
-import { validCmds } from "./validCmds";
+import { isValid, validCmds } from "./validCmds";
 
 const help = (helpCmd: string): void => {
   if (helpCmd === "") {
     console.log("Command, description. Enter 'iok help <command>' for detail");
+    console.log("Note '?' means optional value");
     console.log("---------------------------------------------------");
     for (const [key, value] of validCmds) {
       console.log("<" + key + ">: " + value[0]);
@@ -12,11 +13,12 @@ const help = (helpCmd: string): void => {
     return;
   }
 
-  if (validCmds.has(helpCmd)) {
+  if (isValid(helpCmd)) {
     const cmdDataArr: [string, string] | undefined = validCmds.get(helpCmd);
     if (cmdDataArr !== undefined) {
       console.log("Help for '" + helpCmd + "':");
       console.log(cmdDataArr[0]);
+      console.log();
       console.log(cmdDataArr[1]);
     }
   }
--- src/commands/validCmds.ts
+++ src/commands/validCmds.ts
@@ -1,56 +1,61 @@
 interface CommandData {
   key: string;
-  value: [string, number];
+  value: [string, string];
 }
 
-// valid commands, key=command, value[0]=desc, value[1]=max length valid input (including cmd)
 const commandData: CommandData[] = [
   {
     key: "login",
-    value: ["Login to Clockify. Usage: login username password", 3],
+    value: [
+      "Login to your Clockify account with an API key",
+      "iok login <API_key>",
+    ],
   },
   {
     key: "start",
     value: [
-      "Starts the timer for the currently selected project. Optional: start <project_name>",
-      2,
+      "Starts the timer for selected project, get project names with 'iok list'",
+      "iok start <projectName>",
     ],
   },
   {
     key: "stop",
     value: [
-      "Stops the timer for the currently running timer. If no timer is running, returns no timer running",
-      1,
+      "Stops timer for the currently running timer. If no timer is running, returns no timer running",
+      "iok stop",
     ],
   },
   {
     key: "info",
     value: [
       "Displays information about the current selected project and session time",
-      1,
+      "iok info insert help msg",
     ],
   },
   {
     key: "project",
     value: [
       "Select or create a project. Usage: project <project_name> or project list",
-      2,
+      "iok project insert help msg",
     ],
   },
   {
     key: "help",
-    value: ["insert help msg", 2],
+    value: ["List available commands", "iok help ?commandName"],
   },
   {
     key: "list",
-    value: ["List projects on current workspace", 1],
+    value: [
+      "List projects and their index values on current workspace",
+      "iok list ?indexValue",
+    ],
   },
 ];
 
 // map of valid commands created by mapping the above array.
-const validCmds: Map<string, [string, number]> = new Map<
+const validCmds: Map<string, [string, string]> = new Map<
   string,
-  [string, number]
+  [string, string]
 >(commandData.map((obj) => [obj.key, obj.value]));
 
 const isValid = (cmd: string): boolean => {
