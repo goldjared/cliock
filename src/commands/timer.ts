d550a929edaee4cedf4a9a1fae234cd95db7b764
jared <114547220+goldjared@users.noreply.github.com>
2024-02-29 19:52:40 -0500

    update import/export names

--- src/commands/timer.ts
+++ src/commands/timer.ts
@@ -1,57 +1,14 @@
 import { readData, writeData } from "../sys/fileOps";
 import type {
   UserData,
-  ProjectList,
   PostedTimeResponse,
 } from "../clockifyApi/validationTypes";
 import { clockifyResponse, genPostReqOptions } from "../clockifyApi/validation";
-import { parseTimeDuration } from "./timerUtil";
-
-const getWorkspaceTimeUrl = (wrkspcId: string): string =>
-  `https://api.clockify.me/api/v1/workspaces/${wrkspcId}/time-entries`;
-
-const getProjectId = (projectName: string, projList: ProjectList): string => {
-  const projListLength: number = Object.keys(projList).length;
-
-  // iterate through project list, return proj ID if name param exists in projList
-  for (let i = 0; i < projListLength; i++) {
-    if (projList[i].name === projectName) return projList[i].id;
-  }
-
-  // projName/ID does not exist. Return blank string
-  return "";
-};
-
-const getUTCTimeNow = (): string => {
-  const date = new Date();
-  const year: string = date.getUTCFullYear().toString();
-  let month: string = date.getUTCMonth().toString();
-  let day: string = date.getUTCDate().toString();
-  let hours: string = date.getUTCHours().toString();
-  let minutes: string = date.getUTCMinutes().toString();
-  let seconds: string = date.getUTCSeconds().toString();
-
-  // yyyy-MM-ddThh:mm:ssZ
-  if (month.length < 2) month = "0" + month;
-  if (day.length < 2) day = "0" + day;
-  if (hours.length < 2) hours = "0" + hours;
-  if (minutes.length < 2) minutes = "0" + minutes;
-  if (seconds.length < 2) seconds = "0" + seconds;
-  return (
-    year +
-    "-" +
-    month +
-    "-" +
-    day +
-    "T" +
-    hours +
-    ":" +
-    minutes +
-    ":" +
-    seconds +
-    "Z"
-  );
-};
+import {
+  parseTimeDuration,
+  getWorkspaceTimeUrl,
+  getUTCTimeNow,
+} from "./util/timerUtil";
 
 const start = (projectId: string): void => {
   const data = readData();
@@ -125,4 +82,4 @@ const stop = (): void => {
   writeData(JSON.stringify(userDataJson));
 };
 
-export { getProjectId, start, stop, isTimerRunning };
+export { start, stop, isTimerRunning };
